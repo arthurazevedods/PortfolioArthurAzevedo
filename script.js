b79bc1b91c96aafdd9a4ba28dea9181c36b9a411
arthur <arthurazevedods@gmail.com>
2024-03-06 22:51:56 -0300

    scrollUp at links to

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ links.forEach(function (link) {
             elemento.scrollIntoView({ behavior: 'smooth', block: 'start' });
             setTimeout(function () {
                 window.scrollBy(0, -50);
-            }, 2000);
+            }, 1000);
         }
     });
 });
